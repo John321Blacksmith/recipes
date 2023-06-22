9cefe94a7a30a81ad04a267b094fbbf70c3db59b
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-22 14:11:25 +0300

    the recipe & comment endpoint handlers have been added

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Recipe, Comment
+from .serializers import RecipeSerializer, CommentSerializer
 # Create your views here.
+
+
+# The views below facilitate
+# both collection & object API endpoints
+# of recipes.
+class RecipeList(generics.ListAPIView):
+	queryset = Recipe.objects.all()
+	serializer_class = RecipeSerializer
+
+
+class RecipeDetail(generics.RetrieveAPIView):
+	queryset = Recipe.objects.all()
+	serializer_class = RecipeSerializer
+
+
+# The view below facilitates
+# collection API endpoints
+# of comments.
+class CommentList(generics.ListAPIView):
+	queryset = Comment.objects.all()
+	serializer_class = CommentSerializer
