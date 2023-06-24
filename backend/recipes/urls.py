01c125474a8b008c8b10a7882acf83704a301f0f
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-24 19:30:22 +0300

    added formats suffixes

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-# from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 # router = routers.DefaultRouter()
@@ -9,7 +9,9 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-	path('recipes/', views.RecipeList.as_view()),
+	path('recipes/', views.recipe_list),
 	path('recipes/<int:pk>/', views.recipe_detail),
 	path('recipes/<int:pk>/comments/', views.recipe_comments),
-] 
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
