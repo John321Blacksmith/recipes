70bdc11a282da1dd3f019cb7db4385f20f039d9f
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-24 17:03:45 +0300

    added customized user admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
 
+class CustomUserAdmin(UserAdmin):
+	add_form = CustomUserCreationForm
+	form = CustomUserChangeForm
+	model = CustomUser
+	list_display = [
+		'email',
+		'username',
+		'nickname',
+		'is_staff',
+	]
+	fileldsets = UserAdmin.fieldsets + ((None, {'fields': ('nickname',)}),)
+	add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('nickname',)}),)
+
+
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
