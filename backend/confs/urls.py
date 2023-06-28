8ace5e03ce9146352b8b92cf2809ce4ded9fbf5a
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-28 15:45:02 +0300

    added user collection & detail endpoint

--- backend/confs/urls.py
+++ backend/confs/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('recipes.urls')),
+    path('users/', include('users.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
 ]
