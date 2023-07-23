9b61818758e2785e642df4fabdbfe7b92df0a7e4
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-07-23 16:44:02 +0300

    specified params for celery instance

---
+++ backend/confs/celery.py
@@ -0,0 +1,11 @@
+"""
+Initialize a celery istance and define both settings
+and a list of tasks the selery workers would perform.
+"""
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'confs.settings')
+app = Celery('confs')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks() 
