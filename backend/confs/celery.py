fc65d4023eafa09435ccfd0da6b269a57380abee
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-10-07 14:01:22 +0300

    continued

--- backend/confs/celery.py
+++ backend/confs/celery.py
@@ -8,4 +8,4 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'confs.settings')
 app = Celery('confs')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks() 
+app.autodiscover_tasks()
