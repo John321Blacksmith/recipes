42ec2c25cf96dcbab22dc32d398fb9cf300f6b61
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-10-28 15:34:18 +0300

    deleted chunky lines

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-# router = routers.DefaultRouter()
-# router.register('recepies', views.RecipeViewSet)
-# router.register('comments', views.CommentViewSet)
 
 app_name = 'recipes'
 
