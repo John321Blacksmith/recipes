329677b267e52c6a570b304a3c797fc308bb49fb
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-11-29 15:56:14 +0300

    deleted models, endpoints, tests

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 app_name = 'users'
@@ -10,5 +9,3 @@ urlpatterns = [
 	path('<int:pk>/recipes/', views.UserRecipesList.as_view()),
 	path('<int:pk>/comments/', views.UserCommentsList.as_view()),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
