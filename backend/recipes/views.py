9b3e2b0182f8f0d6e39824fdcd24175ff228ff87
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-11-29 17:21:43 +0300

    the MainPage view got working

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,19 +1,24 @@
 from django.http import Http404
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.generics import GenericAPIView
-from rest_framework.renderers import JSONRenderer
+from rest_framework.filters import SearchFilter
+from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.mixins import ListModelMixin
+from rest_framework.renderers import JSONRenderer, HTMLFormRenderer
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Category, Recipe, Comment
 from .serializers import CategorySerializer, RecipeSerializer, CommentSerializer
 
 
-class MainPageAPIView(GenericAPIView):
+class MainPageAPIView(ListAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     renderer_classes = [JSONRenderer]
-    filter_backends = [DjangoFilterBackend]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title']
     
-
+    
 class CategoryRecipesAPIView(GenericAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
