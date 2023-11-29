5b9d36112c454b91edeb78d909bd316ce82f1238
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-11-29 17:23:39 +0300

    tests have not been passed yet

--- backend/recipes/tests.py
+++ backend/recipes/tests.py
@@ -1,63 +1,61 @@
 from django.test import TestCase
+from django.utils.timezone import datetime
 from django.contrib.auth import get_user_model
-from .models import Recipe, Comment
-
+from .models import Recipe, Comment, Category
 # Create your tests here.
 
-
-class Recipe_and_Comment_model_data_test(TestCase):
-	"""
-	In this test the models Recipe and Comment
-	are checked if anything works as expected.
-	"""
-
-	@classmethod
-	def setUpTestData(cls):
-		cls.recipe_author = get_user_model().objects.create_user(
-					username='test user',
-					email='test@mail.com',
-					password='test123',
-			)
-
-		cls.another_user = get_user_model().objects.create(
-					username='another user',
-					email='test2@mail.com',
-					password='test1234',
-			)
-
-		cls.recipe = Recipe.objects.create(
-					title='test',
-					ingredients='test, test',
-					category='testing',
-					instruction='test instruction',
-					author=cls.recipe_author,
+class TestResipeLifeSpan(TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.user = get_user_model().objects.create_user(
+            username='test',
+            email='test@mail.com',
+            password='test123'
+        )
+        cls.reviewer = get_user_model().objects.create_user(
+            username='reviewer',
+            email='reviewer@mail.com',
+            password='reviewpassword'      
+        )
+        cls.category = Category.objects.create(title='Test category')
+        cls.recipe = Recipe.objects.create(category=cls.category, 
+                                           author=cls.user,
+                                           title='Test',
+                                           description='Testtesttest',
+                                           date_published=datetime(2023, 11, 29, 13, 30, 55, 278206),
+                                           prep_time=10.0,
+                                           cook_time=30.0,
+                                           directions='testhatprogram'
+                                        )
+        cls.comment = Comment.objects.create(
+			recipe=cls.recipe,
+			author=cls.reviewer,
+			content='great test',
+			date_published=datetime.now()
 		)
+        
 
-		cls.comment = Comment.objects.create(
-					author=cls.another_user,
-					recipe=cls.recipe,
-					content='that is a good test',
-			)
 
-	def test_recipe_model_data(self):
-		"""
-		Check if the recipe model data
-		is saved correctly.
-		"""
-		self.assertEqual(self.recipe.title, 'test')
-		self.assertEqual(self.recipe.ingredients, 'test, test')
-		self.assertEqual(self.recipe.category, 'testing')
-		self.assertEqual(self.recipe.instruction, 'test instruction')
-		self.assertEqual(self.recipe.author.username, 'test user')
 
-	def test_comment_model_data(self):
-		"""
-		Check if the comment model
-		data is saved correctly.
-		"""
-		
-		self.assertEqual(self.comment.author.username, 'another user')
-		self.assertEqual(self.comment.recipe, self.recipe)
-		self.assertEqual(self.comment.content, 'that is a good test')
 
-# passed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
