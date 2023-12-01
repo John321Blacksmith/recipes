337669c55f438706484ed2bc0834f0f5928ad1c7
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-01 18:05:25 +0300

    deleted IngredientsTable

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Recipe, Comment, Category, IngredientsTable, Ingredient
+from .models import Recipe, Comment, Category, Ingredient
 # Register your models here.
 
 admin.site.register(Recipe)
 admin.site.register(Comment)
 admin.site.register(Category)
-admin.site.register(IngredientsTable)
 admin.site.register(Ingredient)
 
 
