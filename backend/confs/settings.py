35ca0584bd0bc3f7be33f93ea064f6cb174b6e79
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-01 18:05:54 +0300

    multiple migrs & fixes

--- backend/confs/settings.py
+++ backend/confs/settings.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'confs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
     }
 }
 
