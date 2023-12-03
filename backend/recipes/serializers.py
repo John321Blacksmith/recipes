7769ea93e433f2529f25cfa19743ce54604f4eba
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-03 19:04:28 +0300

    added List & Detail API View for Recipe

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -8,14 +8,52 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['title']
 
 
-class RecipeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Recipe
-        fields = '__all__'
-    
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
 
+
+class RecipeListSerializer(serializers.ModelSerializer):
+    comments_amount = serializers.SerializerMethodField('get_comments_amount')
+    class Meta:
+        model = Recipe
+        fields = [
+            'pk',
+            'category',
+            'author',
+            'title',
+            'description',
+            'date_published',
+            'prep_time',
+            'cook_time',
+            'directions',
+            'comments_amount'
+        ]
+    
+    def get_comments_amount(self, obj):
+        return len(obj.comments.all())
+    
+
+class RecipeDetailSerializer(serializers.ModelSerializer):
+    comments = serializers.SerializerMethodField('get_comments')
+    class Meta:
+        model = Recipe
+        fields = [
+            'pk',
+            'category',
+            'author',
+            'title',
+            'description',
+            'date_published',
+            'prep_time',
+            'cook_time',
+            'directions',
+            'comments'
+        ]
+
+    def get_comments(self, obj):
+        return [
+            dict(comment) for comment in\
+                CommentSerializer(obj.comments.all(), many=True).data
+        ]
