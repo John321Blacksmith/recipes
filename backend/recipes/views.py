dd6d37a4b3292a1f3c2c0b81b430a97c550624aa
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-06 17:02:20 +0300

    deleted useless logic

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.filters import SearchFilter
-from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.renderers import JSONRenderer
 from django_filters.rest_framework import DjangoFilterBackend
@@ -59,65 +59,9 @@ class CategoryRecipesAPIView(ListModelMixin, GenericAPIView):
             return Response(serializer.data, status.HTTP_200_OK)
         return Response(status.HTTP_404_NOT_FOUND)
 
-        
-class RecipeDetailAPIView(GenericAPIView):
-    """
-    This view shows the details of a
-    particular recipe with a comments
-    section with a comment form inside.
-    """
+ 
+class RecipeDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Recipe.objects.all()
-    renderer_classes = [JSONRenderer]
+    serializer_class = RecipeDetailSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get_object(self, request, pk):
-        try:
-            recipe = Recipe.objects.get(pk=pk)
-            return recipe
-        except Recipe.DoesNotExist:
-            return None
-
-    def get(self, request, pk, *args, **kwargs):
-        self.recipe = self.get_object(request, pk)
-        if self.recipe:
-            serializer = self.get_serializer(Recipe)(self.recipe)
-            return Response(serializer.data, status.HTTP_200_OK)
-        return Response(status.HTTP_404_NOT_FOUND)
-    
-    def post(self, request, pk):
-        serializer = self.get_serializer(Comment)(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        recipe = self.get_object(request, pk)
-        if recipe:
-            recipe.delete()
-            return Response(status.HTTP_204_NO_CONTENT)
-        return Response(status.HTTP_403_FORBIDDEN)
-    
-    def put(self, request, pk):
-        recipe = self.get_object(request, pk)
-        if recipe:
-            serializer = self.get_serializer(Comment)(data=request.data)
-            serializer.update()
-            return Response(serializer.data, status.HTTP_201_CREATED)
-        return Response(status.HTTP_403_FORBIDDEN)
-    
-    def get_serializer(self, model):
-        return type(
-                f"{str(model)[str(model).index('.')+1:-2]}Serializer",
-                (serializers.ModelSerializer,),
-                {
-                    'Meta':
-                        type(
-                            'Meta',
-                            (object,),
-                                {
-                                    'model': model, 'fields': '__all__'
-                                }
-                        )
-                }
-            )
+    renderer_classes = [JSONRenderer]
