64d9ba6d71ab4112fa1020f66d0f907f67a8dd7c
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-11 12:34:18 +0300

    added endpoint for comment creation

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.MainPageAPIView.as_view()),
     path('<str:category>/', views.CategoryRecipesAPIView.as_view()),
     path('recipes/<int:pk>/', views.RecipeDetailAPIView.as_view()),
+    path('recipes/<int:pk>/new_comment', views.CommentCreationAPIView.as_view()),
 ]
