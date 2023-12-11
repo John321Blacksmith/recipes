7ba58399c20c9a84af360b7331359927f1cb25ad
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-11 12:34:55 +0300

    CommentCreationAPIView has been added

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.renderers import JSONRenderer
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Category, Recipe, Comment
@@ -61,7 +61,37 @@ class CategoryRecipesAPIView(ListModelMixin, GenericAPIView):
 
  
 class RecipeDetailAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    This view shows up a single recipe
+    details and a list of related comments.
+    """
     queryset = Recipe.objects.all()
     serializer_class = RecipeDetailSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     renderer_classes = [JSONRenderer]
+
+
+class CommentCreationAPIView(GenericAPIView):
+    """
+    This view deals with the Comment
+    creation process only and is invoked
+    via a separate url.
+    """
+    queryset = Recipe.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def post(self, request, *args, **kwargs):
+        """
+        This method works with a new comment
+        object assembled through the frontend
+        form.
+        """
+        return self.create(request, *args, **kwargs)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
