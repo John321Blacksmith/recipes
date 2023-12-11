08a4aafab6add0c3b534ccdea42d6b4eee69ad72
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-11 15:25:17 +0300

    Comment & Recipe creation views have been added

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -9,7 +9,7 @@ from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.renderers import JSONRenderer
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Category, Recipe, Comment
-from .serializers import RecipeListSerializer, RecipeDetailSerializer, CommentSerializer
+from .serializers import RecipeListSerializer, RecipeDetailSerializer, CommentSerializer, RecipeCreationSerializer
 
 
 class MainPageAPIView(ListAPIView):
@@ -95,3 +95,24 @@ class CommentCreationAPIView(GenericAPIView):
             serializer.save()
             return Response(serializer.data, status.HTTP_201_CREATED)
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    
+
+class RecipeCreationAPIView(GenericAPIView):
+    """
+    Ths view provides a Recipe creation form
+    and processes the form data.
+    """
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeCreationSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    
