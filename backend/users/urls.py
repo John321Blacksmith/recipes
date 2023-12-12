96f6176b4bca7306cb36d5381989b963096b8a9a
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-12 17:20:47 +0300

    added endpoint to the recipe creation

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from recipes import views
+
 
 app_name = 'users'
 urlpatterns = [ 
-        path('user', views.View)
+        path('profile/new_recipe/', views.RecipeCreationAPIView.as_view()),
     ]
