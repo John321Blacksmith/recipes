9c1f3be79c5c42c5517482e7f0d3b6d3a3fe4ab3
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-19 17:00:56 +0300

    user profile api

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,15 @@
-from django.urls import path
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.generics import GenericAPIView
+from .serializers import CustomUserSerializer
+from .models import CustomUser
 # Create your views here.
 
 
-class View:
-    ...
+class UserProfileAPIView(GenericAPIView):
+    """
+    This view shows up a profile
+    info with the related content
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes  = [IsAuthenticatedOrReadOnly]
