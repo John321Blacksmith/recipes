8accd57751c9dd753316289b775a25a81b309dcc
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-20 12:05:24 +0300

    user profile api

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,15 +1,19 @@
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.generics import GenericAPIView
 from .serializers import CustomUserSerializer
 from .models import CustomUser
 # Create your views here.
 
 
-class UserProfileAPIView(GenericAPIView):
+class UserProfileAPIView(RetrieveModelMixin, GenericAPIView):
     """
     This view shows up a profile
     info with the related content
     """
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes  = [IsAuthenticatedOrReadOnly]
+    permission_classes  = [IsAuthenticated]
+    
+    def get(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
