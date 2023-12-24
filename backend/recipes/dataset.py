6dcf076bc9b24ef0e861801dae825e3a90a8b789
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-24 13:12:49 +0300

    fixes and migrs.

---
+++ backend/recipes/dataset.py
@@ -0,0 +1,8 @@
+categories = {
+    'meat': {'pork', 'beef', 'stake', 'jusy', 'sauce', 'red', 'blood', 'wellness'},
+    'side dish': {'potato', 'spaghetti', 'vegetables', 'boiled', 'rice'},
+    'fish': {'fish', 'marine', 'red', 'caviar', 'salmon', 'stake'},
+    'diet': {'health', 'water', 'vitamin', 'vegetable', 'protein'},
+    'deserts': {'sweet', 'candies', 'tea', 'cofee', 'chocolate', 'ice-cream'},
+    'pastry': {'cake', 'dough', 'flash', 'soft', 'fragrant', 'warm'}
+}
