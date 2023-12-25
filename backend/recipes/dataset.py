9742628edb4e4b7ba2da396bcc9aad331b2d44eb
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-25 10:39:20 +0300

    dataset was updated

--- backend/recipes/dataset.py
+++ backend/recipes/dataset.py
@@ -1,8 +1,9 @@
 categories = {
-    'meat': {'pork', 'beef', 'stake', 'jusy', 'sauce', 'red', 'blood', 'wellness'},
+    'meat': {'tender', 'stake', 'vine', 'loin', 'pig', 'fat', 'pork', 'ladder', 'pink', 'beef', 'stake', 'jusy', 'sauce', 'red', 'blood', 'wellness'},
     'side dish': {'potato', 'spaghetti', 'vegetables', 'boiled', 'rice'},
     'fish': {'fish', 'marine', 'red', 'caviar', 'salmon', 'stake'},
-    'diet': {'health', 'water', 'vitamin', 'vegetable', 'protein'},
-    'deserts': {'sweet', 'candies', 'tea', 'cofee', 'chocolate', 'ice-cream'},
-    'pastry': {'cake', 'dough', 'flash', 'soft', 'fragrant', 'warm'}
+    'diet': {'health', 'water', 'vitamin', 'vegetable', 'protein', 'cereal', 'oat', },
+    'deserts': {'sweet', 'candies', 'tea', 'cofee', 'chocolate', 'ice-cream', 'sugar', 'caramel'},
+    'pastry': {'cake', 'dough', 'flash', 'soft', 'fragrant', 'warm'},
+    'beverages': {'water', 'fuzzy drinks', 'alcohol', 'soft', 'honey', 'fragrant', 'sugar', 'cocoa', 'fresh', 'mint', 'drink', 'thirsty'}
 }
